09f6b386f04e9bd6f58419894b95fbff19d861f0
Ahmad Rassya Maulana <rrassya19@gmail.com>
2026-07-27 09:55:10 +0700

    Add description field definition to Komik model in komik.js

--- models/komik.js
+++ models/komik.js
@@ -10,6 +10,10 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
         },
 
+        description: {
+            type: DataTypes.STRING,
+        },
+
         
     });
     return Komik;
